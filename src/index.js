5d191eab8ceb630bea4b3d1370ccdb0b22541980
Dhaval Tank <76875509+DhavalTank01@users.noreply.github.com>
2022-03-25 18:31:59 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import ReactDOM from "react-dom";
-// import App from "./MenuApp/App";
-// import UseState from "./IncrDecr/UseState";
-// import UseReducer from "./IncrDecr/UseReducer";
-// import App from "./ToDoApp/App";
-import App from "./WeatherApp/App";
+import App from "./ToDoApp/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
